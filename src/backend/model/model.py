ed59a41e4292789b819ecdee5402a07c19dee3a5
VictorVillacis <vnvillacis@gmail.com>
2021-02-15 14:01:04 -0500

    Main Training Loop Creation

--- src/backend/model/model.py
+++ src/backend/model/model.py
@@ -1,9 +1,8 @@
 from tensorflow import keras
-import tensorflow_addons as tfa
 import tensorflow as tf
 
 
-class VAPORASR(keras.Model):
+class VAPORASR(tf.Module):
 
     def get_config(self):
         pass
@@ -15,12 +14,14 @@ class VAPORASR(keras.Model):
         self.conv = keras.layers.Conv2D(filters=64, kernel_size=5)
         self.lstm = keras.layers.LSTM(units=64, return_sequences=True)
         self.dense = keras.layers.Dense(units=29, activation='sigmoid')
+
         return
 
-    def call(self, inputs, training=None, mask=None):
+    @tf.Module.with_name_scope
+    def __call__(self, inputs, training=False, mask=None):
         x = self.conv(inputs)
         x = tf.transpose(x, perm=[0, 2, 1, 3])
-        x = tf.reshape(x, (x.shape[0], x.shape[1], x.shape[2] * x.shape[3]))
+        x = tf.reshape(x, [x.shape[0], -1, x.shape[2] * x.shape[3]])
         x = self.lstm(x)
         x = self.dense(x)
 
--- src/backend/train.py
+++ src/backend/train.py
@@ -1,24 +1,46 @@
+# import os
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 from data.data_loader import SpeechDataset
 import tensorflow as tf
-from tensorflow.keras import backend as K
 from model.model import VAPORASR
+from tensorflow.keras.optimizers import Adam
 
-tf_sess = tf.Session()
-K.set_session(tf_sess)
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.allow_growth = True
+
+
+@tf.function(experimental_relax_shapes=True)
+def train_step(model, optimizer, x, x_len, y, y_len):
+    with tf.GradientTape() as tape:
+        out = model(x, training=True)
+        loss = tf.nn.ctc_loss(y, out, x_len, y_len, logits_time_major=False)
+
+    grads = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+    return loss
 
 
 def train(opt: dict):
 
     train_dataset = SpeechDataset(opt['train_path'], options['batch_size'])
+    num_batches = len(train_dataset)
     train_dataset = tf.data.Dataset.from_generator(train_dataset,
                                                    output_signature=train_dataset.get_output_tensor_signatures())
 
     model = VAPORASR()
-    tmp = list(train_dataset.take(1))
-    x, x_len, y, y_len = tmp[0]
-    out = model(x)
+    optimizer = Adam()
 
-    loss = tf.nn.ctc_loss(y, out, x_len, y_len, logits_time_major=False)
+    for epoch in range(opt['Epochs']):
+
+        train_dataset = train_dataset.shuffle(buffer_size=64)
+
+        for step, batch in enumerate(train_dataset):
+            x, x_len, y, y_len = batch
+            train_step(model, optimizer, x, x_len, y, y_len)
+
+            print(step)
 
     return
 
@@ -29,7 +51,7 @@ if __name__ == "__main__":
     options['train_path'] = "data/raw/Dev/"
     options['test_path'] = "data/raw/Dev/"
     options['batch_size'] = 8
-
+    options['Epochs'] = 8
     train(options)
 
     pass
