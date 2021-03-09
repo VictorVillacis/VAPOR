61c761f77e2abac09725fd9aa8b4c97736c4c962
VictorVillacis <vnvillacis@gmail.com>
2021-03-08 22:53:51 -0500

    Encode Decode Docstring

--- src/backend/data/encode_decode.py
+++ src/backend/data/encode_decode.py
@@ -1,7 +1,14 @@
 import numpy as np
 import tensorflow as tf
+from typing import Union
 
-def create_char_dict():
+def create_char_dict() -> (dict, dict):
+    """
+    Creates dictionaries to translate characters to integers, and vice-versa.
+
+    Returns:
+        Character dictionary and inverse character dictionary
+    """
     char_dict_ = dict()
 
     current_char = 'A'
@@ -31,7 +38,16 @@ def create_char_dict():
 char_dict, inv_char_dict = create_char_dict()
 
 
-def str_to_npy_ints(char_str):
+def str_to_npy_ints(char_str: str) -> np.ndarray:
+    """
+    Translates a string to a numpy array of corresponding integers.
+
+    Args:
+        char_str: String of alphabetical characters.
+
+    Returns:
+        Numpy integer translation of input string
+    """
     number_seq = list()
 
     for char in char_str:
@@ -42,8 +58,16 @@ def str_to_npy_ints(char_str):
     return np.asarray(number_seq)
 
 
-def int_sequence_to_str(int_sequence):
+def int_sequence_to_str(int_sequence: Union[tf.Tensor, np.ndarray]) -> str:
+    """
+    Translates a tensor or numpy array of integers into its corresponding string.
 
+    Args:
+        int_sequence: Sequence of integers.
+
+    Returns:
+        String of translated integer sequence.
+    """
     if type(int_sequence) == tf.Tensor:
         int_sequence = int_sequence.numpy()
     char_array = []
